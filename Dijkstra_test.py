4b0840ea4f53a6b8df593bb42beb071cdabc0390
Samsoum41 <hocine.samy.sh@gmail.com>
2021-10-12 17:20:01 +0200

    Adjacancy list to matrix test, and vice-versa

---
+++ Dijkstra_test.py
@@ -0,0 +1,27 @@
+"""
+Here is a test file of the Dijkstra algorithm, with graph described by adjacency list or matrix.
+
+To test the solution, I use graphs and results from this page : http://dominique.frin.free.fr/terminales/exosTES-Dijkstra-cor.pdf
+Note that I have replaced letters by numbers (A->0, B->1...) :
+
+A = 0
+B = 1
+C = 2
+D = 3
+E = 4
+F = 5
+G = 6
+"""
+import unittest
+import Dijkstra 
+
+class TestDijkstraFunctions(unittest.TestCase):
+    def test_from_adjacancyList_to_adjacancyMatrix(self):
+        self.assertEqual(Dijkstra.graph1_matrix, Dijkstra.from_adjacencyList_to_adjacencyMatrix(Dijkstra.graph1_list))
+        self.assertEqual(Dijkstra.graph2_matrix, Dijkstra.from_adjacencyList_to_adjacencyMatrix(Dijkstra.graph2_list))
+    def test_from_adjacancyMatrix_to_adjacancyList(self):
+        self.assertEqual(Dijkstra.graph1_list, Dijkstra.from_adjacencyMatrix_to_adjacencyList(Dijkstra.graph1_matrix))
+        self.assertEqual(Dijkstra.graph2_list, Dijkstra.from_adjacencyMatrix_to_adjacencyList(Dijkstra.graph2_matrix))
+
+if __name__ == '__main__':
+    unittest.main()
