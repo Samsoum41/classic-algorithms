b71a441fd565c41743d71746cf28c387c4682adf
Samsoum41 <hocine.samy.sh@gmail.com>
2021-10-12 17:30:17 +0200

    Two dijkstra tests written

--- Dijkstra_test.py
+++ Dijkstra_test.py
@@ -22,6 +22,10 @@ class TestDijkstraFunctions(unittest.TestCase):
     def test_from_adjacancyMatrix_to_adjacancyList(self):
         self.assertEqual(Dijkstra.graph1_list, Dijkstra.from_adjacencyMatrix_to_adjacencyList(Dijkstra.graph1_matrix))
         self.assertEqual(Dijkstra.graph2_list, Dijkstra.from_adjacencyMatrix_to_adjacencyList(Dijkstra.graph2_matrix))
+    def test_dijkstra(self):
+        self.assertEqual((12,[0,4,3,5,6]), Dijkstra.dijkstra(Dijkstra.graph1_matrix))
+        self.assertEqual((37,[0,2,3,5,6]))
+
 
 if __name__ == '__main__':
     unittest.main()
